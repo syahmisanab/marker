e50ecf31c0ccf40638f3f30ddab7529febfa1dc0
Syaz <115781685+syahmisanab@users.noreply.github.com>
2025-06-11 01:35:56 +0800

    Update basket_approach_node.py

--- script/basket_approach_node.py
+++ script/basket_approach_node.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# Date: 2025-06-11  Basket approach based on rectangle tracking
 
 import rospy
 import time
@@ -14,7 +13,6 @@ class BasketApproachNode:
         self.object_info = None
         self.running = True
 
-        # Subscribe to the basket tracking output
         rospy.Subscriber('/object/pixel_coords', ObjectsInfo, self.object_callback)
         rospy.on_shutdown(self.shutdown)
         rospy.loginfo("BasketApproachNode started.")
@@ -22,7 +20,6 @@ class BasketApproachNode:
     def object_callback(self, msg):
         if msg.data:
             self.object_info = msg.data[0]
-            rospy.logdebug(f"Detected object_info: {self.object_info}")
 
     def shutdown(self):
         rospy.loginfo("BasketApproachNode shutting down.")
@@ -30,47 +27,37 @@ class BasketApproachNode:
         self.running = False
 
     def run(self):
-        # Parameters (tune these as needed)
         image_width = 160
-        center_tolerance = 20          # pixels off-center allowed
-        close_area_threshold = 3000    # area at which we consider "close enough"
-        rate = rospy.Rate(10)          # 10 Hz loop
+        center_tolerance = 20
+        close_area_threshold = 3000
+        rate = rospy.Rate(10)
 
         while not rospy.is_shutdown() and self.running:
             if self.object_info:
-                # Extract center and size
-                center_x = self.object_info[2][0]
-                width   = self.object_info[3][0]
-                height  = self.object_info[3][1]
-                area    = width * height
+                center_x = self.object_info.x
+                width    = self.object_info.width
+                height   = self.object_info.height
+                area     = width * height
 
                 image_center_x = image_width / 2
 
-                # Check if close enough
                 if area > close_area_threshold:
-                    rospy.loginfo("? Reached basket (area {:.0f}). Stopping.".format(area))
+                    rospy.loginfo("✅ Reached basket (area: {}). Stopping.".format(area))
                     self.gait_manager.stop()
 
-                # Need to turn left
                 elif center_x < image_center_x - center_tolerance:
-                    rospy.loginfo("? Turning left. center_x={:.0f}".format(center_x))
-                    # mode=2: walking, forward=0, sidestep=0, rotate negative = left
+                    rospy.loginfo("← Turning left. center_x={}".format(center_x))
                     self.gait_manager.move(2, 0, 0, -3)
 
-                # Need to turn right
                 elif center_x > image_center_x + center_tolerance:
-                    rospy.loginfo("? Turning right. center_x={:.0f}".format(center_x))
-                    # positive rotate = right
+                    rospy.loginfo("→ Turning right. center_x={}".format(center_x))
                     self.gait_manager.move(2, 0, 0, 3)
 
-                # Aligned: move forward
                 else:
-                    rospy.loginfo("? Aligned. Moving forward.")
-                    # small forward speed
+                    rospy.loginfo("↑ Aligned. Moving forward.")
                     self.gait_manager.move(2, 0.01, 0, 0)
 
             else:
-                # No detection: stop and wait
                 rospy.loginfo_throttle(5, "…No basket detected; standing by.")
                 self.gait_manager.stop()
 
